3ae96f1307449c8fb4026342adcd3f46e4127f6c
ubu64 <ubu64@ubu64DVL-bergel>
2014-05-21 19:56:50 +0200

    Versio 0.4-B del projecte prjava03-Bergel. Branca master modificada

--- src/prjava03/bergel/Prjava03Bergel.java
+++ src/prjava03/bergel/Prjava03Bergel.java
@@ -7,10 +7,12 @@
 package prjava03.bergel;
 
 
- import java.awt.*; // using AWT containers and components
+
+import java.awt.*; // using AWT containers and components
 
 import java.awt.event.*; // using AWT events and listener interfaces
 
+import java.net.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame
 
 public class Prjava03Bergel extends Frame
